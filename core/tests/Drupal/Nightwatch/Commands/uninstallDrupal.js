9802598ac68e25aee932c2eb83fc1225ab164763
Daniel Wehner <dawehner@googlemail.com>
2018-03-07 12:42:36 +0000

    Allow to specify a dbprefix and make it optional

--- core/tests/Drupal/Nightwatch/Commands/uninstallDrupal.js
+++ core/tests/Drupal/Nightwatch/Commands/uninstallDrupal.js
@@ -1,25 +1,28 @@
 import { execSync } from 'child_process';
 import { commandAsWebserver } from '../globals';
 
-exports.command = function uninstallDrupal(callback) {
+exports.command = function uninstallDrupal(dbPrefix, callback) {
   const self = this;
 
-  if (self.drupalDbPrefix) {
-    const dbPrefix = self.drupalDbPrefix;
+  if (typeof dbPrefix === 'function') {
+    callback = dbPrefix;
+    dbPrefix = '';
+  }
+  dbPrefix = dbPrefix || self.drupalDbPrefix;
+
+  // Nightwatch doesn't like it when no actions are added in command file.
+  const dbOption = process.env.DRUPAL_TEST_DB_URL.length > 0 ? `--db_url ${process.env.DRUPAL_TEST_DB_URL}` : '';
+  try {
+    execSync(commandAsWebserver(`php ./scripts/test-site.php tear-down ${dbOption} ${dbPrefix}`));
+  }
+  catch(error) {
+    this.assert.fail(error);
     // Nightwatch doesn't like it when no actions are added in command file.
-    const dbOption = process.env.DRUPAL_TEST_DB_URL.length > 0 ? `--db_url ${process.env.DRUPAL_TEST_DB_URL}` : '';
-    try {
-      execSync(commandAsWebserver(`php ./scripts/test-site.php tear-down ${dbOption} ${dbPrefix}`));
-    }
-    catch(error) {
-      this.assert.fail(error);
-      // Nightwatch doesn't like it when no actions are added in command file.
-      this.pause(200);
-    }
+    this.pause(200);
   }
 
   if (typeof callback === 'function') {
-    callback.call(self);
+    callback.call(self, dbPrefix);
   }
   return this;
 };
